c147355bd304681a69c05363355239f131e390f0
Gremaud Loïc <flamelegrems@gmail.com>
2018-10-16 12:59:13 +0200

    added python3 and google script

---
+++ google script/script.js
@@ -0,0 +1,47 @@
+function value_points(pos, numberEgality, duration, numberTeam, playerPerTeam, calculationMethod) {
+    if(pos > numberTeam || pos < 1){
+        return 0;
+    }
+    var result = 0;
+    if(calculationMethod == "AVG"){
+        numberEgality = numberEgality + 1;
+    }
+    else if(calculationMethod == "MAX"){
+        numberEgality = 1;
+    }
+    else if(calculationMethod == "MIN"){
+        pos = pos + numberEgality;
+        numberEgality = 1;
+    }
+    number_same_pos = Math.min(Math.max(numberEgality, 1), 256);
+    for(i=0; i<number_same_pos; i++)
+    {
+        var a = Math.log(numberTeam * (playerPerTeam / 2.0) + 15.0 + playerPerTeam) / Math.log(25.0);
+        var b = Math.log(1.0 + Math.pow(duration, 2)) / Math.log(1.05);
+        var c = 100.0 / (90.0 + 10.0 * playerPerTeam);
+        var d = Math.exp( -1.0 / 4.0 * ( (pos + i - 1) * Math.pow((16.0 / numberTeam), 0.5) + 5.0));
+
+        if(pos + i > numberTeam / 2){
+                result += 0;
+        } 
+        else if(pos + i == numberTeam / 2){
+                result += 1;
+        }
+        else{
+                result += Math.max(Math.round(a * b * c * d), 1);
+        }
+    }
+    return Math.round(result / number_same_pos);
+}
+
+function value_points_tab(pos, numberEgality, duration, numberTeam, playerPerTeam, calculationMethod) {
+    var output = "";
+    if(numberEgality != 0){
+     return "Egality must be 0 for the array"; 
+    }
+    for(j=0; j<numberTeam; j++)
+    {
+        output = output + " " + value_points(j + 1, numberEgality, duration, numberTeam, playerPerTeam);
+    }
+    return output;
+}
---
+++ python 3/script.py
@@ -0,0 +1,38 @@
+import math
+
+def value_points(pos, numberEgality, duration, number_team, player_per_team, calculationMethod):
+    """Calcul de la formule hyper wzf pour les points. Prends en compte les égalités. Dans le cas ou
+    number_same_pos != 1, il faut la moyenne avec tous les points des pos entre pos et pos + number_same_pos
+    """
+    if pos > number_team or pos < 1:
+        return 0
+    result = 0
+    if(calculationMethod == "AVG"):
+        numberEgality = numberEgality + 1
+    elif(calculationMethod == "MAX"):
+        numberEgality = 1
+    elif(calculationMethod == "MIN"):
+        pos = pos + numberEgality;
+        numberEgality = 1
+    number_same_pos = min(max(numberEgality, 1), 256)
+    for i in range(number_same_pos):
+        a = math.log(number_team * (player_per_team / 2.0) + 15.0 + player_per_team, 25.0)
+        b = math.log(1.0 + math.pow(duration, 2), 1.05)
+        c = 100.0 / (90.0 + 10.0 * player_per_team)
+        d = math.exp( -1.0 / 4.0 * ( (pos + i - 1) * math.pow((16.0 / number_team), 0.5) + 5.0))
+
+        if pos + i > number_team / 2:
+            result += 0
+        elif pos + i == number_team / 2:
+            result += 1
+        else:
+            result += max(round(a * b * c * d), 1)
+    return round(result / number_same_pos)
+
+def value_points_tab(numberEgality, duration, numberTeam, playerPerTeam, calculationMethod):
+    output = "";
+    if(numberEgality != 0):
+        return "Egality must be 0 for the array"; 
+    for j in range(numberTeam):
+        output = "{} {}".format(output, value_points(j + 1, numberEgality, duration, numberTeam, playerPerTeam, calculationMethod))
+    return output
